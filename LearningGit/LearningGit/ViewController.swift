e7817997e1b8cd720f88f54734b6fb98d59e5f30
Vladimir Kurdiukov <vladimirkurdiukov@MacBook-Pro-Vladimir-2.local>
2024-06-05 19:53:37 +0300

    Добавил проект и добавил его в Git

---
+++ LearningGit/LearningGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearningGit
+//
+//  Created by Vladimir Kurdiukov on 05.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
